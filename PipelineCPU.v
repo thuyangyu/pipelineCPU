1923ab14f1dbf9718ae361ecfb85f514fdf1204f
thuyangyu <yangyu030000@gmail.com>
2014-12-03 08:19:48 +0800

    wire name complete!

--- PipelineCPU.v
+++ PipelineCPU.v
@@ -25,18 +25,24 @@ module PipelineCPU(
 	
 	
 	//wires after PC
-	//5 wires
+	//1 + 2 + 2 wires
 	wire [15:0] PCValue;
+	
 	wire [15:0] PCPlus;
 	wire [15:0] instruction_a_IM;
-	
+
 	wire [15:0] instruction_b_IFID;
 	wire [15:0] PC_b_IFID;
 	
 	//wires after IFID
+	//2+3+14+3 wires
 	wire [15:0] PC_a_IFID;
 	wire [15:0] instruction_a_IFID;
 
+	wire PCWrite;
+	wire IFIDWrite;
+	wire addBubble;
+	
 	wire writeSpecReg_a_Decoder;
 	wire memToReg_a_Decoder;
 	wire regWrite_a_Decoder;
@@ -52,13 +58,13 @@ module PipelineCPU(
 	wire readSpecReg_a_Decoder;
 	wire imSrcSelect;
 	
-	wire outData1AfterRegisters;
-	wire outData2AfterRegisters;
-	wire ExtendedImmediateAfterSE;
+	wire outData1_a_Registers;
+	wire outData2_a_Registers;
+	wire ExtendedImmediate_a_SE;
 
-	
-	
+
 	//wires after ID/EX
+	//13 + 7 + 2 + 7
 	wire writeSpecReg_a_IDEX;
 	wire memToReg_a_IDEX;
 	wire regWrite_a_IDEX;
@@ -72,98 +78,75 @@ module PipelineCPU(
 	wire regDst_a_IDEX;
 	wire branch_a_IDEX;
 	wire readSpecReg_a_IDEX;
+	
 	wire PC_a_IDEX;
 	wire outData1_a_IDEX;
 	wire outData2_a_IDEX;
 	wire ExtendedImmediate_a_IDEX;
-	wire RxAfter_a_IDEX;
-	wire RyAfter_a_IDEX;
-	wire RzAfter_a_IDEX;
+	wire Rx_a_IDEX;
+	wire Ry_a_IDEX;
+	wire Rz_a_IDEX;
+	
+	wire inData1_b_ALU;
+	wire inData2_b_ALU;
 	
 	wire regWrite_b_EXMEM;
 	wire memWrite_b_EXMEM;
 	wire PC_b_EXMEM;
 	wire dataIn_b_EXMEM;
 	wire ALUResult_b_EXMEM;
-	wire TValue_b_EXMEM;
+	wire Zerobit_b_EXMEM;
 	wire registerToWriteId_b_EXMEM;
 	
-	wire inData1_b_ALU;
-	wire inData2_b_ALU;
-	
 	//wires after EX/MEM
+	//5 + 6 + 1 + 1 wires
 	wire writeSpecReg_a_EXMEM;
 	wire memToReg_a_EXMEM;
 	wire regWrite_a_EXMEM;
 	wire memRead_a_EXMEM;
 	wire memWrite_a_EXMEM;
-	wire PC_a_EXMEM;
+	
 	wire branch_a_EXMEM;
-	wire TValue_a_EXMEM;
+	wire PC_a_EXMEM;
+	wire Zerobit_a_EXMEM;
 	wire ALUResult_a_EXMEM;
 	wire dataIn_a_EXMEM;
 	wire registerToWriteId_a_EXMEM;
+	
+	wire PCSrc;
+	
+	wire dataOut_a_MemController;
+	
 	//wires after MEM/WB
+	//3 + 3 + 1 wires
+	wire writeSpecReg_a_MEMWB;
+	wire memToReg_a_MEMWB;
+	wire regWrite_a_MEMWB;
 	
-	wire c_WB_regWrite_a_Decoder;
-	wire c_WB_memtoReg_a_Decoder;
-	wire c_MEM_memRead_a_Decoder;
-	wire c_MEM_memWrite_a_Decoder;
-	wire c_MEM_branch_a_Decoder;
-	wire c_EX_ALUSrc_a_Decoder;
-	wire c_EX_ALUOp_a_Decoder;
-	wire c_EX_regDst_a_Decoder;
-	
-	
-	wire c_WB_regWrite_a_IDEX;
-	wire c_WB_memtoReg_a_IDEX;
-	wire c_MEM_memRead_a_IDEX;
-	wire c_MEM_memWrite_a_IDEX;
-	wire c_MEM_branch_a_IDEX;
-	wire c_EX_ALUSrc_a_IDEX;
-	wire c_EX_ALUOp_a_IDEX;
-	wire c_EX_regDst_a_IDEX;
-	
-	
-
-	
-	wire c_WB_regWrite_a_EXMEM;
-	wire c_WB_memtoReg_a_EXMEM;
-	wire c_MEM_memRead_a_EXMEM;
-	wire c_MEM_memWrite_a_EXMEM;
-	wire c_MEM_branch_a_EXMEM;
-	
-
-	wire registerToWriteIdAfterEXMEM;
+	wire dataOut_a_MEMWB;
+	wire ALUResult_a_MEMWB;
+	wire registerToWriteId_a_MEMWB;
 	
 	wire dataToWriteBack;
-	wire dataAfterMC;
+
+	//************************************* start attachment
 	
-	wire c_WB_regWrite_a_MEMWB;
-	wire c_WB_memtoReg_a_MEMWB;
-	wire dataAfterMEMWB;
-	wire ALUResultAfterMEMWB;
-	wire registerToWriteIdAfterMEMWB;
-	
+	//modules in PC stage
 	//PC module
     reg [15:0] PC;
 	assign PCValue[15:0] = PC[15:0];
-	
+	assign PCPlus[15:0] = PCValue[15:0] + 16'b1;        //temp add 1;
+	assign PC_b_IFID[15:0]               = IFIDWrite ? PC_a_IFID[15:0] : PCPlus[15:0];  //mux
+	assign instruction_b_IFID[15:0] = IFIDWrite ? instruction_a_IFID[15:0] : ((jump || PCSrc) ? 16b0000_1000_0000_0000 : instruction_a_IM[15:0]);
 	//Instruction_Memory module
-    wire [15:0] instruction;
     Instruction_Memory im(
         .CLK(CLK),
         .RST(RST),
         .address(PCValue),
-        .instruction(instruction)
+        .instruction(instruction_a_IM)
         ); 
 	
-	//Add  1 to PC
-	assign PCPlus[15:0] = PCValue[15:0] + 16'b1;        //temp add 1;
-	
-	
-	wire [15:0] instructionAfterIFID;
-	wire [15:0] PCAfterIFID;
+	//modules in IFID
 	
 	//IF_ID
 	IF_ID if_id(
