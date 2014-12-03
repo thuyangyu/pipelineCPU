8ef645aee3821c22a044060292bb5fe53fd365c0
YiqingZhang <ichingchang@foxmail.com>
2014-12-03 14:57:07 +0800

    finish the sign extender AND change = to <= in ALU and forwardUnit

--- ALU.v
+++ ALU.v
@@ -17,69 +17,69 @@ begin
 
     4'b0000:
     begin
-    	result = first + second;
+    	result <= first + second;
     end 
 
     4'b0001:
     begin
-        result = first - second;
+        result <= first - second;
     end 
 
     4'b0010:
     begin
-        result = first & second;
+        result <= first & second;
     end 
 
     4'b0011:
     begin
-        result = first | second;
+        result <= first | second;
     end 
 
     4'b0100:
     begin
-        result = ~first;
+        result <= ~first;
     end 
 
     4'b0101:
     begin
-        result = first >>> second;
+        result <= first >>> second;
     end
 
     4'b0110:
     begin
-        result = first << second;
+        result <= first << second;
     end
 
     4'b0111:
     begin
-        if(first < second) result = 16'b1;
-        else result = 16'b0; 
+        if(first < second) result <= 16'b1;
+        else result <= 16'b0; 
     end
 
     4'b1000:
     begin
-        result = 16'b0;
+        result <= 16'b0;
     end 
 
     4'b1001:
     begin
-        result = first;
+        result <= first;
     end 
 
     4'b1010:
     begin
-        result = !first;
+        result <= !first;
     end 
 
     4'b1011:
     begin
-        if(first == second) result = 16'b0;
-        else result = 16'b1;
+        if(first == second) result <= 16'b0;
+        else result <= 16'b1;
     end 
 
     4'b1100:
     begin
-        result = second;
+        result <= second;
     end 
 
     endcase
--- ForwardUnit.v
+++ ForwardUnit.v
@@ -1,16 +1,16 @@
 `timescale 1ns / 1ns
 
 module ForwardUnit(
-    input [2:0] Rx_a_IDEX;
-    input [2:0] Ry_a_IDEX;
-    input [2:0] Rz_a_IDEX;
-    input regWrite_a_EXMEM;
-    input regWrite_a_MEMWB;
-    input [2:0] registerToWriteId_a_EXMEM;
-    input [2:0] registerToWriteId_a_MEMWB;
-    input writeSpecReg_a_EXMEM;
-    input writeSpecReg_a_MEMWB;
-    input readSpecReg_a_IDEX;
+    input [2:0] Rx_a_IDEX,
+    input [2:0] Ry_a_IDEX,
+    input [2:0] Rz_a_IDEX,
+    input regWrite_a_EXMEM,
+    input regWrite_a_MEMWB,
+    input [2:0] registerToWriteId_a_EXMEM,
+    input [2:0] registerToWriteId_a_MEMWB,
+    input writeSpecReg_a_EXMEM,
+    input writeSpecReg_a_MEMWB,
+    input readSpecReg_a_IDEX,
 
     output [1:0] forward1,
     output [1:0] forward2
