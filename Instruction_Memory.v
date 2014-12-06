f61c408868faae3bddbb79c8bdbe6ff1812c42f9
ZhangYiqing <zhang-yq10@mails.tsinghua.edu.cn>
2014-12-06 17:30:17 +0800

    change the instruction memory to a version without state machine

--- Instruction_Memory.v
+++ Instruction_Memory.v
@@ -15,86 +15,23 @@ module Instruction_Memory(
     inout [15:0] RAM2DATA
 );
 
-//these are the buffers of the outputs
-reg OEBuffer;
-reg WEBuffer;
-reg ENBuffer;
 
-reg [15:0] ADDRBuffer;
-
-
-//assign those buffers
-assign RAM2OE = OEBuffer;
-assign  RAM2WE = WEBuffer;
-assign RAM2EN = ENBuffer;
-assign RAM2ADDR[17:0] = {2'b0, ADDRBuffer[15:0]};
+assign RAM2OE = CLK;
+assign RAM2WE = 1'b1;//always disable
+assign RAM2EN = 1'b0;//always enable
+assign RAM2ADDR[17:0] = {2'b0, address[15:0]};
 assign RAM2DATA[15:0] =  16'bZZZZ_ZZZZ_ZZZZ_ZZZZ;
-
-
-
-parameter   S0 = 1'b0,
-S1 = 1'b1;
-
-reg state;//you can not assign the register value outside of the always block
-reg nextState;
 
 always @ (posedge CLK, negedge RST)
 begin
-    if(!RST)begin
-
-        OEBuffer <= 1'b1;
-		WEBuffer <= 1'b1;
-		ENBuffer <= 1'b1;
-		ADDRBuffer[15:0] <= 16'b0;
-        
-    end
-    else begin
-        case(state)
-        S0:begin
-            ENBuffer <= 1'b0;//the 0 is enable
-            WEBuffer <= 1'b1;
-            OEBuffer <= 1'b1;
-            ADDRBuffer[15:0] <= address[15:0];
+    if(!RST)
+        begin
+            instruction[15:0] <= 16'b0000_1000_0000_0000;
         end
-        
-        S1:begin
-            ENBuffer <= 1'b0;//the 0 is enable
-            WEBuffer <= 1'b1;
-            OEBuffer <= 1'b0;
-            //ADDRBuffer[15:0] <= address[15:0];
-            instruction[15:0] <= RAM2DATA[15:0]; 
-            
+    else
+        begin
+            instruction[15:0] <= RAM2DATA[15:0];
         end
-        
-        endcase
-    end
-
 end
-
-
-//control the next state
-always @(posedge CLK, negedge RST)
-begin
-    if(!RST)begin
-        nextState = S0;
-    end
-    else begin
-        case(state)
-            S0:begin
-                nextState = S1;
-            end
-            
-            S1:begin
-                nextState = S0;
-            end
-        endcase
-    end
-end
-
-always @(posedge CLK) 
-begin
-	state <= nextState;
-end
-
 
 endmodule
